8fa870eba916140c326c487b6cfca62ac794286f
ZEESHAN IFTIKHAR <mzeshann1@gmail.com>
2024-12-02 19:24:50 +0500

    adding the tasks files

--- wp-content/themes/twentytwentyfive/page-kanye-quotes.php
+++ wp-content/themes/twentytwentyfive/page-kanye-quotes.php
@@ -2,12 +2,13 @@
 /*
 Template Name: Kanye Quotes
 */
-get_header();  // Load the header
 ?>
+
+<?php get_header(); ?>
 
 <div class="kanye-quotes">
     <h1>Kanye West Quotes</h1>
     <ul id="kanye-quotes-list">Loading quotes...</ul>
 </div>
 
-<?php get_footer();  // Load the footer ?>
+<?php get_footer();?>
